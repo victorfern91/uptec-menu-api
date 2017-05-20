2d63bbc61878ed0d8d8e19ba7fee175bdd5bef3b
Victor Fernandes <victorfern91@gmail.com>
2017-05-20 21:27:10 +0100

    Add index.js filem and removed some logic from url.js

--- menu-parser/modules/url.js
+++ menu-parser/modules/url.js
@@ -2,25 +2,14 @@ const moment = require('moment');
 // menu-parser constants
 const URL = require('../globals').urlParameters;
 
-function getMenuForCurrentWeek() {
-  const momentInstance = moment()
-  return getUrl(momentInstance);
-}
-
-function getMenuForWeek(weekNumber) {
-  const momentInstance = moment().week(weekNumber);
-  return getUrl(momentInstance);
-}
-
-function getUrl(momentInstance) {
-  const year = momentInstance.year();
-  const month = momentInstance.format('MM');
-  const firstDay = momentInstance.day('monday').format('DD-MM-YYYY');
-  const lastDay = momentInstance.day('friday').format('DD-MM-YYYY');
+function getMenuURLForCurrentWeek() {
+  const year = moment().year();
+  const month = moment().format('MM');
+  const firstDay = moment().day('monday').format('DD-MM-YYYY');
+  const lastDay = moment().day('friday').format('DD-MM-YYYY');
   return `http://www.${URL.domain}/${year}/${month}/${URL.prefix}-${firstDay}-${URL.to}-${lastDay}${URL.extension}`;
 }
 
 module.exports = {
-  getMenuForCurrentWeek,
-  getMenuForWeek
+  getMenuURLForCurrentWeek
 }
